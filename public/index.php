b1a1a536a894f9006fdf1f12639fb8abb43ac8f0
Saqlain khadim hw <saqlainkhadim@letshirepro.com>
2022-07-16 16:43:20 +0500

    blade 1/10

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Jenssegers\Blade\Blade;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -9,10 +10,24 @@ require __DIR__.'/../vendor/autoload.php';
 $app= AppFactory::create();
 
 
+function view(Response $response,$template,$with=[]){
+    $cache =__DIR__.'../cache';
+    $views =__DIR__.'../resources/views';
+
+    $blade=( new Blade($views,$cache))->make($template,$with);
+    $response->getBody()->write($blade->render());
+    return $response;
+}
+
+
 $app->addErrorMiddleware(true,true,true);
 $app->get('/',function (Request $request, Response $response, $parameters){
     $response->getBody()->write("Boring Slim framework");
     return $response;
+
+});
+$app->get('/home',function (Request $request, Response $response, $parameters){
+    return view($response,'abc.abc');
 
 });
 $app->run();
